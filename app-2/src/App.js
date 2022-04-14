764b1a1c1649a8270284c65d9d44bb26511c4eac
Alex Lamb <alexlamb104@gmail.com>
2022-04-13 20:33:42 -0600

    Finished app-2, displayed array on screen as a list

--- app-2/src/App.js
+++ app-2/src/App.js
@@ -1,26 +1,27 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import testingLibraryJestDom from '@testing-library/jest-dom'
 
 function App() {
+  const [food, setFood] = useState([
+    "Bacon",
+    "Bread",
+    "Lettuce",
+    "Tomato",
+    "Mayo",
+  ]);
+  
+  const foodList = food.map((ingredient) => {
+    return <p>{ingredient}</p>
+  })
+
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <div className="App">
+    <h1>Food List</h1>
+    {foodList}
+  </div>
+);}
 
 export default App;
