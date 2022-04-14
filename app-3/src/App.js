8bc9bc4b0213869c7254c96fe6d7098e50690e3f
Alex Lamb <alexlamb104@gmail.com>
2022-04-13 21:04:52 -0600

    Completed app-3, filter list based on user input

--- app-3/src/App.js
+++ app-3/src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
 
 function App() {
+  const [food, setFood] = useState([
+    "spaghetti",
+    "ice cream",
+    "sushi",
+    "bologna",
+    "cheese",
+    "pizza",
+  ]);
+  const [userInput, setUserInput] = useState("");
+
+  const handleChange = (e) => {
+    setUserInput(e.target.value);
+  };
+
+  let foodList = food
+    .filter((index) => {
+      return index.includes(userInput);
+    })
+    .map((i) => {
+      return <h2>{i}</h2>;
+    });
+  // const handleChange()
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <input type="text" onChange={handleChange} />
+      <p>{foodList}</p>
     </div>
   );
 }
