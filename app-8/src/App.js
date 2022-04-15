1b464f6986d3012176e25680dbb61cfa5911c415
Alex Lamb <alexlamb104@gmail.com>
2022-04-15 12:26:19 -0600

    completed app-8 use API to display info on screen

--- app-8/src/App.js
+++ app-8/src/App.js
@@ -1,24 +1,20 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import axios from "axios";
 
 function App() {
+  const [info, setInfo] = useState([]);
+
+  axios.get("https://pokeapi.co/api/v2/pokemon/35/").then((res) => {
+    setInfo(res.data);
+  });
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>{info.name}</h1>
+      <h3>Height: {info.height}</h3>
+      <h3>Weight: {info.weight}</h3>
     </div>
   );
 }
